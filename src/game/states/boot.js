2403d263df712143d40925414bb2e64ea87743d1
FoxChrisRealTheGit <cfox1290@gmail.com>
2018-02-28 16:12:26 -0700

    just needs logic

--- src/game/states/boot.js
+++ src/game/states/boot.js
@@ -1,10 +1,8 @@
 export default function bootState(){
     return{
-        preload:function(){
-
-        },
         create: function(){
-            
+            this.game.physics.startSystem(window.Phaser.Physics.ARCADE);
+            this.game.state.start('load');
         }
     }
 }
--- src/game/states/load.js
+++ src/game/states/load.js
@@ -4,16 +4,18 @@ export default function loadState() {
             var loadingLabel = this.game.add.text(80, 150, 'loading...',
                 { font: '30px Courier', fill: '#fff' });
 
-            this.game.scale.scaleMode = window.Phaser.ScaleManager.SHOW_ALL;
+            this.game.scale.scaleMode = window.Phaser.ScaleManager.NO_SCALE;
             this.game.scale.pageAlignHorizontally = true;
             this.game.scale.pageAlignVertically = true;
             //game.stage.backgroundColor = '#eee';
 
            
             this.game.load.spritesheet('button', 'img/greySheet.png', 190, 47.3)
+            this.game.load.spritesheet('board', 'img/wood.png', 32, 32)
+            this.game.load.spritesheet('checkers', 'img/checker.png', 32, 32)
         },
         create: function () {
-            this.game.state.start('menu')
+            this.game.state.start('play')
         }
     }
 }
